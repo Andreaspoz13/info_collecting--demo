6fa7d291a76d2bb03bcaeefcb5b43a40d6b67f7a
Andreaspoz13 <99254946+Andreaspoz13@users.noreply.github.com>
2022-07-13 22:48:35 +0300

    source code and executable file

---
+++ collct_info_demo/collct_info_demo/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there, whats your name?");
+
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("Hello there," + userName + "isn't that a good day?");
+            var goodDay_user = Console.ReadLine();
+
+            Console.WriteLine("Yeah me too!");
+
+            Console.ReadKey();
+            Console.WriteLine("How old are you?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("So you are" + userAge + " ?");
+            Console.ReadLine();
+
+            Console.WriteLine("Here are the results:/n Username:" + userName + "Age:" + userAge);
+
+
+
+
+
+        }
+    }
+}
